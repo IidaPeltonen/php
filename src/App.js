87c6cd9f7c2fa998e2c317d516c7bc65ad5e2c75
IidaPeltonen <n0peii00@students.oamk.fi>
2021-10-07 17:37:17 +0300

    alku

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect,useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [message, setMessage], useSate('');
+
+  useEffect(()=> {
+
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Message from web-server</h3>
     </div>
   );
 }
