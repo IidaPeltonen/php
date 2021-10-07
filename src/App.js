6a6a4a04f6936b16c0689f6c2d2a0f057375a008
IidaPeltonen <n0peii00@students.oamk.fi>
2021-10-07 17:52:12 +0300

    alku

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
-import {useEffect,useState} from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [message, setMessage], useSate('');
+  const [message, setMessage] = useState('');
 
   useEffect(()=> {
-
+    axios.get(URL)
+      .then((response) => {
+        setMessage(response.data);
+      }).catch(error => {
+        alert(error);
+      });
   }, [])
   return (
     <div className="App">
