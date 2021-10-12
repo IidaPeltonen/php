24c1aa450fcb78a299c01dd417400e8cb1a75153
IidaPeltonen <n0peii00@students.oamk.fi>
2021-10-12 12:04:31 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
+const URL = 'http://localhost/php1/index.php';
+
 function App() {
   const [message, setMessage] = useState('');
-
+  const [origin, setOrigin] = useState('');
   useEffect(()=> {
     axios.get(URL)
       .then((response) => {
-        setMessage(response.data);
+        setMessage(response.data.message);
+        setOrigin(response.data.origin);
       }).catch(error => {
         alert(error);
       });
@@ -16,6 +19,9 @@ function App() {
   return (
     <div className="App">
       <h3>Message from web-server</h3>
+      <p>{message}</p>
+      <h3>Your address is</h3>
+      <p>{origin}</p>
     </div>
   );
 }
